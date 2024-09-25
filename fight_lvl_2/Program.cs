b8ad69ff62cb549923ff434d9cfec231e36bb575
alexander swedlund <alexander.swedlund@elev.ga.ntig.se>
2024-09-25 14:49:12 +0200

    tewrjmkwerh

--- fight_lvl_2/Program.cs
+++ fight_lvl_2/Program.cs
@@ -9,7 +9,8 @@ int block2cooldown = 0;
 int dmg = 0;
 int dmg2 = 0;
 int round = 0;
-Console.WriteLine("punch does normal damage   kick does dubble damage but you take dubble damage this turn cooldown 2 turns  block blocks all damage this turn cooldown 2 turns");
+int childaction = 0;
+Console.WriteLine("punch does normal damage   kick does dubble damage but you take dubble damage this turn and you have to wait one turn until you can kick again  block blocks all damage this turn and you have to wait one turn until you can block again");
 Console.WriteLine("start fight");
 Console.ReadLine();
 while (childhp > 0 && hp > 0)
@@ -19,87 +20,13 @@ while (childhp > 0 && hp > 0)
     Console.WriteLine();
     dmg = rnd.Next(1, 10);
     dmg2 = rnd.Next(1, 10);
-    if (kickcooldown > 0)
-    {
-        kickcooldown--;
-    }
+    childaction = rnd.Next(1, 3);
 
-    if (blockcooldown > 0)
-    {
-        blockcooldown--;
-    }
 
 
     Console.WriteLine("choose attack");
-    Console.WriteLine("type 1 for punch. 2 for kick. 3 for block");
+    Console.WriteLine("type 1 for punch. 2 for kick. 3 for block ");
     string action = Console.ReadLine();
-    if (block2cooldown < 2 && kick2cooldown < 2)
-    {
-        if (action == "1")
-        {
-            childhp -= dmg;
-        }
-        if (action == "2")
-        {
-            childhp -= dmg * 2;
-            kickcooldown = 2;
-            Console.WriteLine("you kick the child");
-        }
-        if (action == "3")
-        {
-            blockcooldown = 2;
-            Console.WriteLine("you block the child");
-        }
-        else
-        {
-            Console.WriteLine("you skip your turn");
-        }
-    }
-    if (block2cooldown < 2 && kick2cooldown == 2)
-    {
-        if (action == "1")
-        {
-            childhp -= dmg * 2;
-        }
-        if (action == "2")
-        {
-            childhp -= dmg * 4;
-            kickcooldown = 2;
-            Console.WriteLine("you kick the child");
-        }
-        if (action == "3")
-        {
-            blockcooldown = 2;
-            Console.WriteLine("you block the child");
-        }
-        else
-        {
-            Console.WriteLine("you skip your turn");
-        }
-    }
-    if (block2cooldown == 2 && kick2cooldown < 2)
-    {
-        if (action == "1")
-        {
-           
-            Console.WriteLine("you punch the child and the child blocks");
-        }
-        if (action == "2")
-        {
-            
-            kickcooldown = 2;
-            Console.WriteLine("you kick the child and the child blocks");
-        }
-        if (action == "3")
-        {
-            blockcooldown = 2;
-            Console.WriteLine("you block the child and the child blocks");
-        }
-        else
-        {
-            Console.WriteLine("you skip your turn and the child blocks");
-        }
-    }
     if (block2cooldown > 0)
     {
         block2cooldown--;
@@ -108,23 +35,149 @@ while (childhp > 0 && hp > 0)
     {
         kick2cooldown--;
     }
+    if (childaction == 2)
+    {
+        if (kick2cooldown > 0)
+        {
+            childaction = 1;
+        }
+        else
+        {
+            if (blockcooldown == 2)
+            {
+                Console.WriteLine("you block the childs kick");
+            }
+            if (kickcooldown == 2 && blockcooldown < 2)
+            {
+                Console.WriteLine("the child kicks you");
+                hp -= dmg2 * 4;
+            }
+            if (blockcooldown < 2 && kickcooldown < 2)
+            {
+                Console.WriteLine("the child kicks you");
+                hp -= dmg2 * 2;
+            }
+            kick2cooldown = 2;
+        }
 
-    Console.WriteLine();
-    Console.WriteLine("child punch you");
-    hp -= dmg2;
-    Console.WriteLine($"child hp = {childhp}");
-    Console.WriteLine($"your hp = {hp}");
-    Console.ReadLine();
+    }
+
+    if (childaction == 3)
+    {
+        if (block2cooldown > 0)
+        {
+            childaction = 1;
+        }
+        else
+        {
+            Console.WriteLine("the child blocks you");
+            block2cooldown = 2;
+        }
+
+    }
+
+
+
+    if (childaction == 1)
+    {
+        if (blockcooldown < 2 && kickcooldown < 2)
+        {
+            Console.WriteLine("the child punches you");
+            hp -= dmg;
+        }
+        if (blockcooldown == 2)
+        {
+            Console.WriteLine("the child punches you and you block");
+        }
+        if (blockcooldown < 2 && kickcooldown == 2)
+        {
+            Console.WriteLine("the child punches you ");
+            hp -= dmg * 2;
+        }
+    }
+     if (kickcooldown > 0)
+    {
+        kickcooldown--;
+    }
+
+    if (blockcooldown > 0)
+    {
+        blockcooldown--;
+    }
+    if (block2cooldown < 2 && kick2cooldown < 2)
+    {
+        if (action == "1")
+        {
+            childhp -= dmg;
+            Console.WriteLine("you punch the child");
+        }
+        if (action == "2" && kickcooldown == 0)
+        {
+            childhp -= dmg * 2;
+            kickcooldown = 2;
+            Console.WriteLine("you kick the child");
+        }
+        if (action == "3" && blockcooldown == 0)
+        {
+            blockcooldown = 2;
+            Console.WriteLine("you block the child");
+        }
+    }
+   
+    if (block2cooldown < 2 && kick2cooldown == 2)
+    {
+        if (action == "1")
+        {
+            childhp -= dmg * 2;
+            Console.WriteLine("you punch the child");
+        }
+        if (action == "2" && kickcooldown == 0)
+        {
+            childhp -= dmg * 4;
+            kickcooldown = 2;
+            Console.WriteLine("you kick the child");
+        }
+        if (action == "3" && blockcooldown == 0)
+        {
+            blockcooldown = 2;
+            Console.WriteLine("you block the child");
+        }
+
+
+    }
+    if (block2cooldown == 2 && kick2cooldown < 2)
+    {
+        if (action == "1")
+        {
+
+            Console.WriteLine("you punch the child and the child blocks");
+        }
+        if (action == "2" && kickcooldown == 0)
+        {
+
+            kickcooldown = 2;
+            Console.WriteLine("you kick the child and the child blocks");
+        }
+        if (action == "3" && blockcooldown == 0)
+        {
+            blockcooldown = 2;
+            Console.WriteLine("you block the child and the child blocks");
+        }
+
+    }
+
+    Console.WriteLine($"your hp {hp}. childs hp {childhp}");
+
 }
-if (childhp > hp)
+if (childhp > 0)
 {
     Console.WriteLine("you lose");
 }
-if (childhp == hp)
+if (childhp < 0 && hp < 0)
 {
     Console.WriteLine("tie");
 }
-if (childhp < hp)
+if (hp > 0)
 {
     Console.WriteLine("you win");
 }
